97cef6788d804dacbbc1c263184ddf3eb455fe7b
YCJ <yuchangjiang@hulianpai.com>
2023-02-28 18:00:25 +0800

    init

--- app/usercenter/internal/service/user.go
+++ app/usercenter/internal/service/user.go
@@ -3,8 +3,30 @@ package service
 import (
 	"context"
 	v1 "kratos-admin/api/usercenter/service/v1"
+	"kratos-admin/app/usercenter/internal/biz"
 )
 
 func (s *UserCenterService) CreateUser(ctx context.Context, req *v1.CreateUserReq) (resp *v1.CreateUserResp, err error) {
+	u := &biz.User{
+		Username: req.Username,
+		Realname: req.Realname,
+		Mobile:   req.Mobile,
+		AreaCode: req.AreaCode,
+		Password: req.Password,
+		Email:    req.Email,
+		Weixin:   req.Weixin,
+		Operator: req.Operator,
+		CreateAt: req.CreateAt.AsTime(),
+		UpdateAt: req.UpdateAt.AsTime(),
+	}
+
+	user, err := s.uc.CreateUser(ctx, u)
+	if err != nil {
+		return nil, err
+	}
+	resp = &v1.CreateUserResp{
+		Id:  int64(user.Id),
+		Uid: user.Uid,
+	}
 	return &v1.CreateUserResp{}, nil
 }
